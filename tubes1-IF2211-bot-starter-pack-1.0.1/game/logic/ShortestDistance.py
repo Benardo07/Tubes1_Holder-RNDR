a2a4af6cb876cbe7c7bdef251b78c4d96b9fb6a1
Filbert <92862064+Filbert88@users.noreply.github.com>
2024-03-06 23:28:28 +0700

    revert

---
+++ tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/ShortestDistance.py
@@ -0,0 +1,101 @@
+import random
+from typing import Optional
+
+from game.logic.base import BaseLogic
+from game.models import GameObject, Board, Position
+from ..util import *
+
+class ShortestDistance(BaseLogic):
+    def __init__(self):
+        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        self.goal_position: Optional[Position] = None
+        self.current_direction = 0
+
+    def next_move(self, board_bot: GameObject, board: Board):
+
+        props = board_bot.properties
+        current_position = board_bot.position
+        teleport = find_teleport(current_position, board)
+        tele_sorted_list = sorted(teleport, key=lambda d: d[0])
+        
+        # Analyze new state
+        if props.diamonds == 5:
+            # Move to base
+            base = board_bot.properties.base
+            self.goal_position = base
+        else:
+            # Just roam around
+            # red_diamond = [x for x in board.game_objects if x.type=="DiamondButtonGameObject"]
+            diamond = find_diamond(current_position, board)
+            red_diamond = find_red_diamond(current_position, board)
+            blue_diamond = find_blue_diamond(current_position, board)
+            red_button = find_red_button(current_position,board)
+
+            sorted_list = sorted(diamond, key=lambda d: d[0])
+            sorted_blue = sorted(blue_diamond, key=lambda d: d[0])
+            sorted_red = sorted(red_diamond, key=lambda d: d[0])
+            sorted_button = sorted(red_button, key=lambda d: d[0])
+            base_distance = abs(current_position.x - board_bot.properties.base.x) + abs(current_position.y - board_bot.properties.base.y)
+            base_distance_tele = tele_sorted_list[0][0] + abs(tele_sorted_list[1][1].x - board_bot.properties.base.x) + abs(tele_sorted_list[1][1].y - board_bot.properties.base.y)
+
+
+            self.goal_position = sorted_list[0][1]
+
+            if(props.diamonds >= 3 and (base_distance <= 6 or base_distance_tele <= 6) and sorted_list[0][0] >= 5):
+                self.goal_position = board_bot.properties.base
+            
+            elif(props.milliseconds_left < 8000 and props.diamonds != 0):
+                self.goal_position = board_bot.properties.base
+
+            elif (props.diamonds >= 3 and (base_distance <= 6 or base_distance_tele <= 6) and sorted_list[0][0] <= 5):
+                self.goal_position = sorted_list[0][1]
+
+            elif (props.diamonds == 4 and sorted_blue != []):
+                self.goal_position = sorted_blue[0][1]
+
+            elif (props.diamonds == 4 and sorted_blue == []) :
+                self.goal_position = board_bot.properties.base
+
+            elif (sorted_blue != [] and sorted_red != [] and props.diamonds < 4):
+                if (sorted_red[0][0] <= sorted_blue[0][0]) :
+                    self.goal_position = sorted_red[0][1]
+
+            # elif ((abs(current_position.x - sorted_list[0][1].position.x) >= 9)or(abs(current_position.y - sorted_list[0][1].position.y) >= 9)) :
+            #     if((abs(current_position.x - sorted_button[0][1].position.x) <= 9)or(abs(current_position.y - sorted_button[0][1].position.y) <= 9)) :
+            #         self.goal_position = sorted_button[0][1]
+            #     elif((abs(current_position.x - sorted_button[0][1].position.x) >= 9)or(abs(current_position.y - sorted_button[0][1].position.y) >= 9)):
+            #         self.goal_position = sorted_list[0][1]
+
+            #     else :
+            #         self.goal_position = sorted_list[0][1]
+                    
+        current_position = board_bot.position
+        if self.goal_position:
+            if(tele_sorted_list):
+                distance_tele2_goal = abs(tele_sorted_list[1][1].x - self.goal_position.x) + abs(tele_sorted_list[1][1].y - self.goal_position.y)
+                way1 = tele_sorted_list[0][0] + distance_tele2_goal
+                
+                way = abs(current_position.x - self.goal_position.x) + abs(current_position.y - self.goal_position.y)
+
+                if(way1 < way):
+                    self.goal_position = tele_sorted_list[0][1]
+            # We are aiming for a specific position, calculate delta
+            delta_x, delta_y = get_direction(
+                current_position.x,
+                current_position.y,
+                self.goal_position.x,
+                self.goal_position.y,
+            )
+        
+        if(delta_x == 0 and delta_y == 0):
+            delta_x, delta_y = random.choice([(1, 0), (0, 1), (-1, 0), (0, -1)])
+        # else:
+        #     # Roam around
+        #     delta = self.directions[self.current_direction]
+        #     delta_x = delta[0]
+        #     delta_y = delta[1]
+        #     if random.random() > 0.6:
+        #         self.current_direction = (self.current_direction + 1) % len(
+        #             self.directions
+        #         )
+        return delta_x, delta_y
